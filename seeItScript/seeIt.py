0e53ec5fa2498faa38e65750d24638d461a72828
SamBarrett1 <samuel.barrett@my.jcu.edu.au>
2021-08-19 13:53:31 +1000

    Added required program structure

--- seeItScript/seeIt.py
+++ seeItScript/seeIt.py
@@ -1,3 +1,26 @@
+# PROGRAM STRUCTURE REQUIRED
+
+# """Module docstring"""
+#
+#
+# # imports
+# # CONSTANTS
+#
+# def main():
+#     """Function docstring"""
+#     # statements...
+#     # variables...
+#     do_stuff()
+#
+#
+# def do_stuff():
+#     """Function docstring"""
+#     # statements...
+#
+#
+# main()
+
+
 # INDEXING
 """
 numbers = [1, 2, 3, 4, 5]
